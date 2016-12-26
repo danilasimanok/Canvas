76d69afff71756834fd229ed9de7748e6dd01a7f
danilasimanok <dsimanok@bk.ru>
2016-12-26 18:43:48 +0300

    работает

--- app/src/main/java/com/example/falyanguzov/canvas/Cannon.java
+++ app/src/main/java/com/example/falyanguzov/canvas/Cannon.java
@@ -1,8 +1,12 @@
 package com.example.falyanguzov.canvas;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -12,12 +16,21 @@ public class Cannon extends View {
     private int alpha;
     private Paint paint = new Paint();
     private int d;
+    private Bitmap yee,mama;
+    private Rect src,mamaSrc;
+    private float x;
+    private float y;
+    private int nmama=0;
 
     public Cannon(Context context) {
         super(context);
         paint.setColor(0x000000FF);
         alpha = 1;
         d = 1;
+        yee= BitmapFactory.decodeResource(getResources(),R.drawable.yee);
+        src=new Rect(0,0,yee.getWidth(),yee.getHeight());
+        mama=BitmapFactory.decodeResource(getResources(),R.drawable.mama);
+        mamaSrc=new Rect(0,0,mama.getWidth(),mama.getHeight());
     }
 
     @Override
@@ -27,8 +40,25 @@ public class Cannon extends View {
         alpha += d;
         int w = getWidth();
         int h = getHeight();
-        paint.setColor(0x1000000*alpha+0xff);
+        paint.setColor(0x1000000*alpha+0xff00);
         canvas.drawCircle(w/2.0f, h/2.0f, h/4.0f, paint);
+        if(nmama>=3){
+            Rect mdst=new Rect((int)x-mama.getWidth()/2,(int)y-mama.getHeight()/2,(int)x+mama.getWidth()/2,(int)y+mama.getHeight()/2);
+            canvas.drawBitmap(mama,mamaSrc,mdst,paint);
+        }
+        else {
+            Rect dst=new Rect((int)x-yee.getWidth()/2,(int)y-yee.getHeight()/2,(int)x+yee.getWidth()/2,(int)y+yee.getHeight()/2);
+            canvas.drawBitmap(yee,src,dst,paint);
+        }
         invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x=event.getX();
+        y=event.getY();
+        if(nmama==6)nmama=0;
+        else nmama++;
+        return super.onTouchEvent(event);
     }
 }
