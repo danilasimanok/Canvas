49d3750de16798d73aacbf7cdbc2fab791a79805
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-12-26 14:36:04 +0300

    Мигание

--- app/src/main/java/com/example/falyanguzov/canvas/Cannon.java
+++ app/src/main/java/com/example/falyanguzov/canvas/Cannon.java
@@ -9,17 +9,26 @@ import android.view.View;
  * Created by falyanguzov on 26.12.2016.
  */
 public class Cannon extends View {
+    private int alpha;
     private Paint paint = new Paint();
+    private int d;
 
     public Cannon(Context context) {
         super(context);
-        paint.setColor(0xFF0000FF);
+        paint.setColor(0x000000FF);
+        alpha = 1;
+        d = 1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        if(alpha==0 || alpha==255)
+            d *= -1;
+        alpha += d;
         int w = getWidth();
         int h = getHeight();
+        paint.setColor(0x1000000*alpha+0xff);
         canvas.drawCircle(w/2.0f, h/2.0f, h/4.0f, paint);
+        invalidate();
     }
 }
